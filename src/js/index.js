6a5c0ea29d97ed402fffae539ac86dd663cbe3cd
Denys Rudenko <69275031+DenysRudenko@users.noreply.github.com>
2024-07-19 02:20:17 +0100

    fix(): changed main divs to sections

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,41 @@
-console.log("hello world");
+let name = "ivan";
+
+let number = 7;
+const pi = 3.14;
+
+number = 4;
+
+let leftBorderWidth = 200;
+
+//number
+
+// let obj = {
+// 	name: 'apple',
+// 	color: 'green',
+// 	weight: 200
+// }
+
+//  Symbol
+
+// let answer = prompt("Are you 18?", "");
+// console.log(answer);
+
+// const num = 50;
+
+// if (num < 49) {
+// 	console.log("Incorrect")
+// } else if (num > 100) {
+// 	console.log("Too many")
+// } else {
+// 	console.log("Correct")
+// }
+
+// for (i = 1; i < 8; i++) {
+// 	console.log(i);
+// }
+
+function logging(a, b) {
+	console.log(a + b)
+}
+
+logging(3, 6);
